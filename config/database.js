aa098f62265017cf1bd98b86b2e19924b6533dbc
Prasadasw <prasadaswar6870@gmail.com>
2024-11-14 15:11:21 +0530

    new changes

--- config/database.js
+++ config/database.js
@@ -1,30 +1,8 @@
-const { Sequelize } = require('sequelize');
-require('dotenv').config();
-
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST || 'localhost',
-  dialect: 'mysql',
-  logging: false, 
-});
-
-sequelize.authenticate()
-  .then(() => {
-    console.log('Connection to the MySQL database has been established successfully.');
-  })
-  .catch((error) => {
-    console.error('Unable to connect to the MySQL database:', error);
-  });
-
-module.exports = sequelize;
-
-
-
-
 // const { Sequelize } = require('sequelize');
 // require('dotenv').config();
 
-// const sequelize = new Sequelize('Dataentry_DB', 'root', 'root', {
-//   host: 'localhost',
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//   host: process.env.DB_HOST || 'localhost',
 //   dialect: 'mysql',
 //   logging: false, 
 // });
@@ -38,3 +16,25 @@ module.exports = sequelize;
 //   });
 
 // module.exports = sequelize;
+
+
+
+
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize('Dataentry_DB', 'root', 'root', {
+  host: 'localhost',
+  dialect: 'mysql',
+  logging: false, 
+});
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection to the MySQL database has been established successfully.');
+  })
+  .catch((error) => {
+    console.error('Unable to connect to the MySQL database:', error);
+  });
+
+module.exports = sequelize;
