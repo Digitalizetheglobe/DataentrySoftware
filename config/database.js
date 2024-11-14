4b7a66c9fbec56e02e0cda209c094bd255178c8a
Prasadasw <prasadaswar6870@gmail.com>
2024-11-14 15:19:09 +0530

    prod

--- config/database.js
+++ config/database.js
@@ -18,23 +18,24 @@
 // module.exports = sequelize;
 
 
-
-
-const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize('Dataentry_DB', 'root', 'root', {
-  host: 'localhost',
-  dialect: 'mysql',
-  logging: false, 
-});
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST || 'localhost',
+    dialect: 'mysql',
+    logging: false,
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_PROD_NAME,
+    host: process.env.DB_HOST || 'localhost',
+    dialect: 'mysql',
+    logging: false,
+  },
+};
 
-sequelize.authenticate()
-  .then(() => {
-    console.log('Connection to the MySQL database has been established successfully.');
-  })
-  .catch((error) => {
-    console.error('Unable to connect to the MySQL database:', error);
-  });
-
-module.exports = sequelize;
