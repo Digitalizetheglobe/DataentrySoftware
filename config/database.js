099ff98bfe1eae90e38c8874059ee640042ad836
Prasadasw <prasadaswar6870@gmail.com>
2024-11-15 14:49:26 +0530

    new

--- config/database.js
+++ config/database.js
@@ -1,41 +1,41 @@
-// const { Sequelize } = require('sequelize');
-// require('dotenv').config();
-
-// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-//   host: process.env.DB_HOST || 'localhost',
-//   dialect: 'mysql',
-//   logging: false, 
-// });
-
-// sequelize.authenticate()
-//   .then(() => {
-//     console.log('Connection to the MySQL database has been established successfully.');
-//   })
-//   .catch((error) => {
-//     console.error('Unable to connect to the MySQL database:', error);
-//   });
-
-// module.exports = sequelize;
-
-
+const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-module.exports = {
-  development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST || 'localhost',
-    dialect: 'mysql',
-    logging: false,
-  },
-  production: {
-    username: process.env.DB_USER, 
-    password: process.env.DB_PASSWORD, 
-    database: process.env.DB_PROD_NAME, 
-    host: process.env.DB_HOST || 'localhost', 
-    dialect: 'mysql',
-    logging: false,
-  }
-};
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST || 'localhost',
+  dialect: 'mysql',
+  logging: false, 
+});
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection to the MySQL database has been established successfully.');
+  })
+  .catch((error) => {
+    console.error('Unable to connect to the MySQL database:', error);
+  });
+
+module.exports = sequelize;
+
+
+// require('dotenv').config();
+
+// module.exports = {
+//   development: {
+//     username: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     host: process.env.DB_HOST || 'localhost',
+//     dialect: 'mysql',
+//     logging: false,
+//   },
+//   production: {
+//     username: process.env.DB_USER, 
+//     password: process.env.DB_PASSWORD, 
+//     database: process.env.DB_PROD_NAME, 
+//     host: process.env.DB_HOST || 'localhost', 
+//     dialect: 'mysql',
+//     logging: false,
+//   }
+// };
 
