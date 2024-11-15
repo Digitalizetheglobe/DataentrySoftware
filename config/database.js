5890f8fe6f26e85584c4b8180cba7b36dd6f2ad1
Prasadasw <prasadaswar6870@gmail.com>
2024-11-15 14:37:32 +0530

    NEW ENV

--- config/database.js
+++ config/database.js
@@ -30,13 +30,12 @@ module.exports = {
     logging: false,
   },
   production: {
-    username: 'root', 
-    password: 'CptSoftware', 
-    database: 'Dataentry_DB', 
-    host: 'localhost', 
+    username: process.env.DB_USER, 
+    password: process.env.DB_PASSWORD, 
+    database: process.env.DB_PROD_NAME, 
+    host: process.env.DB_HOST || 'localhost', 
     dialect: 'mysql',
     logging: false,
   }
-  
 };
 
